58309a2b07e9a444a5e6bbcd80ec948685cbaabd
codeXplorerePo <codexplore.vuchinam97@gmail.com>
2023-04-16 20:41:32 +0700

    NgSwitch

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  public loginName = 'admin';
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
